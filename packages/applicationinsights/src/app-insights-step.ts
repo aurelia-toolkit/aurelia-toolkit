146a70906f93950bf13c67ea07b8adf001e7e237
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-26 13:42:46 +1000

    feat(applicationinsights): add

--- packages/applicationinsights/src/app-insights-step.ts
+++ packages/applicationinsights/src/app-insights-step.ts
@@ -1,20 +1,15 @@
-﻿import * as au from "../aurelia";
-import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+﻿import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { inject } from 'aurelia-framework';
+import { PipelineStep, NavigationInstruction, Next } from 'aurelia-router';
 
-@au.autoinject
-export class AppInsightsStep implements au.PipelineStep {
-	constructor(private appInsights: ApplicationInsights) {
-		this.logger = au.getLogger("AppInsightsStep");
-		this.logger.debug("Created");
-	}
+@inject(ApplicationInsights)
+export class AppInsightsStep implements PipelineStep {
+  constructor(private appInsights: ApplicationInsights) { }
 
-	logger: au.Logger;
-
-	run(navigationInstruction: au.NavigationInstruction, next: au.Next) {
-		let origin = window.location.pathname + window.location.hash;
-		let path = origin.replace("/#/", "/").replace("#", "");
-		this.logger.debug(`Tracking for ${path}`);
-		this.appInsights.trackPageView({ name: navigationInstruction.config.name, uri: path, properties: navigationInstruction.params });
-		return next();
-	}
+  async run(navigationInstruction: NavigationInstruction, next: Next) {
+    const origin = window.location.pathname + window.location.hash;
+    const path = origin.replace('/#/', '/').replace('#', '');
+    this.appInsights.trackPageView({ name: navigationInstruction.config.name, uri: path, properties: navigationInstruction.params });
+    return next();
+  }
 }
