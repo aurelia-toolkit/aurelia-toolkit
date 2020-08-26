00575ccda7b519f479256ab43249e63d14de5973
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-26 15:03:10 +1000

    fix(exception): export

---
+++ packages/exception/src/index.ts
@@ -0,0 +1,8 @@
+export { ExceptionService } from './exception-service';
+export { Exception } from './exception';
+export { ForbiddenException } from './forbidden';
+export { IApiException } from './i-api-exception';
+export { InvalidCredentialsException } from './invalid-credentials';
+export { TooManyRecordsException } from './too-many-records';
+export { UnauthorizedException } from './unauthorized';
+export { UnconfirmedEmailException } from './unconfirmed-email';
