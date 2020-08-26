b6eae45e4fc8da083ade773c8bac70be1b86c19f
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-26 14:55:45 +1000

    fix(alert): export service

--- packages/alert/src/index.ts
+++ packages/alert/src/index.ts
@@ -1,5 +1,7 @@
 import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
+export { AlertService } from './alert-service';
+
 export function configure(frameworkConfiguration: FrameworkConfiguration) {
   frameworkConfiguration.globalResources([
     PLATFORM.moduleName('./global-progress/global-progress'),
