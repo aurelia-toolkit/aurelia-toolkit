090a54361d2c6484ec2e14032732191adf7c8cbb
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-26 15:02:24 +1000

    fix(auth): export service

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,3 +2,5 @@ export { AuthService } from './auth-service';
 export { IJwtToken } from './i-jwt-token';
 export { ITokens } from './i-tokens';
 export { IUsersClient } from './i-users-client';
+export { AuthStatus } from './router-config';
+export { RoleCheckStep, isRouteAllowed } from './role-check-step';
