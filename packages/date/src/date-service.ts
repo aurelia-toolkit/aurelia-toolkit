86229f0b5037f2e2a824a882055431bedb43db0e
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-26 15:02:57 +1000

    fix(date): remove async

--- packages/date/src/date-service.ts
+++ packages/date/src/date-service.ts
@@ -2,19 +2,19 @@ export class DateService {
   differenceWithServer: number;
   initialised: boolean = false;
 
-  async init(serverDate: Date): Promise<void> {
+  init(serverDate: Date) {
     this.differenceWithServer = Date.now() - serverDate.getTime();
     this.initialised = true;
   }
 
-	now(): Date {
-		if (this.differenceWithServer === undefined) {
-			throw Error('Date service is not initialised');
-		}
-		return new Date(Date.now() - this.differenceWithServer);
-	}
+  now(): Date {
+    if (this.differenceWithServer === undefined) {
+      throw Error('Date service is not initialised');
+    }
+    return new Date(Date.now() - this.differenceWithServer);
+  }
 
-	serverToLocal(date: Date): Date {
-		return new Date(date.getTime() + this.differenceWithServer);
-	}
+  serverToLocal(date: Date): Date {
+    return new Date(date.getTime() + this.differenceWithServer);
+  }
 }
