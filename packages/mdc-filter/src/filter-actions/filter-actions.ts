121c5550641258900d5d347883ed1b941406748b
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-28 15:52:42 +1000

    clean(mdc-filter)

--- packages/mdc-filter/src/filter-actions/filter-actions.ts
+++ packages/mdc-filter/src/filter-actions/filter-actions.ts
@@ -1,12 +1,12 @@
 import { bindable, customElement } from 'aurelia-framework';
-import { Filter } from '@aurelia-toolkit/mdc-filter';
+import { Filter } from '../filter';
 
 @customElement('filter-actions')
 export class FilterActions {
   constructor(private element: Element) { }
 
   @bindable
-  filterVm: Filter;
+  filter: Filter;
 
   search() {
     this.element.dispatchEvent(new CustomEvent('search', { bubbles: true }));
