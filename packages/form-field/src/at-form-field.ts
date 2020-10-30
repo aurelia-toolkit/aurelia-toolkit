538baab457d9f477660ef512afae5570bb569552
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-10-30 16:51:31 +1100

    feat(form-field): add width attribute

--- packages/form-field/src/at-form-field.ts
+++ packages/form-field/src/at-form-field.ts
@@ -1,5 +1,14 @@
-import { customElement, useView, PLATFORM } from 'aurelia-framework';
+import { customElement, useView, PLATFORM, bindable, inject } from 'aurelia-framework';
 
+@inject(Element)
 @customElement('at-form-field')
 @useView(PLATFORM.moduleName('./at-form-field.html'))
-export class AtFormField { }
+export class AtFormField {
+  constructor(private element: HTMLElement) { }
+
+  @bindable
+  width: string;
+  widthChanged() {
+    this.element.style.width = `${this.width}px`;
+  }
+}
