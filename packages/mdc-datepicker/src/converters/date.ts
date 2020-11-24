039e91ec5a9be0cb53ded9623240d453cb720299
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-11-25 09:04:08 +1100

    fix(mdc-datepicker): add converter

---
+++ packages/mdc-datepicker/src/converters/date.ts
@@ -0,0 +1,20 @@
+﻿import format from 'date-fns/format';
+
+export class DateValueConverter {
+  // eslint-disable-next-line no-undef
+  toView(value: string | Date, formatStr?: string, locale?: Locale): string {
+    if (!value) {
+      return '';
+    }
+
+    if (typeof value === 'string') {
+      value = new Date(Date.parse(value));
+    }
+
+    if (formatStr) {
+      return format(value, formatStr, { locale });
+    } else {
+      return value.toLocaleDateString();
+    }
+  }
+}
