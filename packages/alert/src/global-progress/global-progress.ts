ea439a203d505376d1c96882ce76d389f9127851
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-05-06 15:12:08 +1000

    fix(global-progress): pass size to mdc progress

--- packages/alert/src/global-progress/global-progress.ts
+++ packages/alert/src/global-progress/global-progress.ts
@@ -1,5 +1,12 @@
 import { customElement, useView, PLATFORM } from 'aurelia-framework';
+import { bindable } from 'aurelia-typed-observable-plugin';
 
 @customElement('global-progress')
 @useView(PLATFORM.moduleName('./global-progress.html'))
-export class GlobalProgress { }
+export class GlobalProgress {
+  @bindable.number
+  size: number = 100;
+
+  @bindable.number
+  strokeWidth: number = 10;
+}
