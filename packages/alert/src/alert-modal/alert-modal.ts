a0e85c83f747705d89f7c5295e39b591f4e3137e
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-05-11 16:50:33 +1000

    fix(alert): add alert modal class

--- packages/alert/src/alert-modal/alert-modal.ts
+++ packages/alert/src/alert-modal/alert-modal.ts
@@ -1,9 +1,14 @@
 import { IAlertModalPayload } from './i-alert-modal-payload';
 import { autoinject, useView, PLATFORM } from 'aurelia-framework';
+import { MdcDialog } from '@aurelia-mdc-web/dialog';
 
 @autoinject
 @useView(PLATFORM.moduleName('./alert-modal.html'))
 export class AlertModal {
+  constructor(private dialog: MdcDialog){
+    this.dialog.root.classList.add('alert-modal');
+  }
+
   payload: IAlertModalPayload;
 
   activate(payload: IAlertModalPayload) {
